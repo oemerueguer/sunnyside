ec3ae49ac34c1d49752fd6c31a3934d6783f3cae
Lodos <o.ueguer@gmail.com>
2021-07-27 19:33:32 +0200

    routing

---
+++ src/Routing.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { Home } from './components/Home'
+
+export default function Routing() {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          {/* <Redirect to="/home" /> */}
+          <Home />
+        </Route>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </>
+  );
+}
