81609fc7216f124c97c2f035ba895617344cca1f
Lodos <o.ueguer@gmail.com>
2021-07-28 19:23:35 +0200

    header

--- src/Routing.js
+++ src/Routing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import  Home  from './components/Home'
 
 export default function Routing() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,25 @@
-import React from 'react'
-import './Header.scss'
-import  Logo from './img/logo.svg' 
+import React from "react";
+import "./Header.scss";
+import Logo from "./img/logo.svg";
+import HeaderPic from './img/image-header.jpg'
 
 export default function Header() {
-    return (
-        <>
-        <div className="heading">
-            
-        <div id="navbar">
-            <nav>
-                <div className="nav-group">
-                    <ul>
-                        <li>About</li>
-                        <li>Services</li>
-                        <li>Projects</li>
-                        <li>Contact</li>
-                    </ul>
-                </div>
-            </nav>
-
+  return (
+    <>
+    <div className="section-banner">
+        <div className="navigation">
+            <div className="Logo">
+                <img src={Logo} alt="Logo" />
+            </div>
+            <ul>
+                <li>About</li>
+                <li>Services</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
         </div>
         </div>
 
-            
-        </>
-    )
+    </>
+  );
 }
