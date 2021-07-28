df98c46db042794d65fed39b671658f032e6119b
Lodos <o.ueguer@gmail.com>
2021-07-28 18:47:20 +0200

    navbar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import './Header.scss'
+import  Logo from './img/logo.svg' 
 
 export default function Header() {
     return (
         <>
-        
+        <div className="heading">
+            
+        <div id="navbar">
+            <nav>
+                <div className="nav-group">
+                    <ul>
+                        <li>About</li>
+                        <li>Services</li>
+                        <li>Projects</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </nav>
+
+        </div>
+        </div>
+
             
         </>
     )
