13af2d1929a9e06f1cd43bb9d92bc42412642754
Lodos <o.ueguer@gmail.com>
2021-07-29 18:43:52 +0200

    first step

--- src/components/Branding.js
+++ src/components/Branding.js
@@ -1,20 +1,21 @@
 import React from "react";
 import Egg from './img/image-transform.jpg';
+import './Branding.scss';
 
 export default function Branding() {
   return (
     <>
       <div className="container">
-        <div className="box">
-          <h1>Transform your brand</h1>
+        <div className="info-box">
+          <h1>Transform your <br/> brand</h1>
           <p>
             We are full-service creative agency specializing in helping brands
             grwo fast. Engage your clients through compelling visuals that do
             most of the marketing for you.
           </p>
-          <button>LEARN MORE</button>
+          <div className="Btn-yellow">LEARN MORE</div>
         </div>
-        <div className="box">
+        <div className="image-box">
             <img src={Egg} alt="Egg with yellow background" />
         </div>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,40 @@
 import React from "react";
 import "./Header.scss";
 import Logo from "./img/logo.svg";
-import HeaderPic from './img/image-header.jpg'
+import ArrowDown from './img/icon-arrow-down.svg';
 
 export default function Header() {
   return (
     <>
-    <div className="section-banner">
+      <div className="section-banner">
         <div className="navigation">
-            <div className="Logo">
-                <img src={Logo} alt="Logo" />
-            </div>
-           <div className="item-group"> 
+          <div className="Logo">
+            <a href="/">
+              <img src={Logo} alt="Logo" />
+            </a>
+          </div>
+          <div className="item-group">
             <ul>
+              <a href="/">
                 <li>About</li>
+              </a>
+              <a href="/">
                 <li>Services</li>
+              </a>
+              <a href="/">
                 <li>Projects</li>
+              </a>
+              <a className="ContactBtn" href="/">
                 <li>Contact</li>
+              </a>
             </ul>
-            </div>
+          </div>
         </div>
+        <h1 className="Heading">We are creatives</h1>
+        <div className="icon-arrow-down">
+          <img src={ArrowDown} alt="arrow-down" />
         </div>
-
+      </div>
     </>
   );
 }
